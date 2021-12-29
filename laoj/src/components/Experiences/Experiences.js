957e2e2f0f2618f6803842231dc740b28f10398e
JeremyLaoo <lao.jeremy@live.fr>
2021-12-29 10:51:47 +0100

    gitignore

--- laoj/src/components/Experiences/Experiences.js
+++ laoj/src/components/Experiences/Experiences.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 import './Experiences.css';
-import Model from './Model';
+import ExpModel from './ExpModel';
 
-import data from './data';
+import data from './ExpData';
 import { MenuExps } from './MenuExp';
 
+import { Button, Modal } from 'react-bootstrap';
 
-const Experiences = () => {
+
+
+function Experiences() {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
         return (
             
@@ -26,14 +34,29 @@ const Experiences = () => {
                                         {/* <button type="button" class="btn btn-primary" onClick={() => getData(item.id, item.title, item.company, item.date, item.location, item.contrat, item.description, item.stack, item.url, item.cName)}>
                                             Launch demo modal
                                         </button> */}
-                                        
+                                        <Button variant="primary" onClick={handleShow}>
+                                            Details
+                                        </Button>
                                 </div>
                                 
                             </div>
                         )
                     })}
                 </div>
-                
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                    <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                    </Modal.Footer>
+                </Modal>
             
 
             </div>
